ae345d78b3df9f71ac719a22748ab420ae919537
PauloAthayde <pags1@aluno.ifnmg.edu.br>
2024-10-11 17:06:16 +0000

    adições

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ class Personagem {
     }
 
     sortearEvento() {
-        const eventos = ['ataque_zumbi', 'sala_silenciosa', 'arma_melhor', 'perda_medicamentos', 'sala_armadilha', 'usar_medicamento'];
+        const eventos = ['ataque_zumbis', 'ataque_infectados', 'sala_silenciosa', 'arma_melhor', 'usar_medicamento'];
         return eventos[Math.floor(Math.random() * eventos.length)];
     }
 
@@ -45,20 +45,24 @@ class Personagem {
         let opcao2 = "";
 
         switch (evento) {
-            case 'ataque_zumbi':
-                const dano = Math.floor(Math.random() * 30) + 10; 
-                this.vida -= dano;
-                mensagem = `Você foi atacado por zumbis e perdeu ${dano} de vida.`;
-                opcao1 = "Atacar os zumbis";
-                opcao2 = "Fugir e perder medicamentos";
+            case 'ataque_zumbis':
+                mensagem = "Você está sendo atacado por zumbis!";
+                opcao1 = "Atacá-los";
+                opcao2 = "Fugir";
+                break;
+
+            case 'ataque_infectados':
+                mensagem = "Infectados estão tentando roubar seus medicamentos!";
+                opcao1 = "Atacá-los";
+                opcao2 = "Fugir";
                 break;
 
             case 'sala_silenciosa':
-                const ganhoMedicamentos = Math.floor(Math.random() * 5);
+                const ganhoMedicamentos = Math.floor(Math.random() * 5) + 1; 
                 this.medicamentos += ganhoMedicamentos;
-                mensagem = `Você encontrou uma sala silenciosa e obteve ${ganhoMedicamentos} medicamentos, mas há armadilhas no caminho.`;
-                opcao1 = "Explorar e arriscar vida";
-                opcao2 = "Sair imediatamente";
+                mensagem = `Você encontrou uma sala silenciosa e obteve ${ganhoMedicamentos} medicamentos.`;
+                opcao1 = "Explorar mais a sala";
+                opcao2 = "Avançar para o próximo andar";
                 break;
 
             case 'arma_melhor':
@@ -66,32 +70,16 @@ class Personagem {
                 this.forca += ganhoForca;
                 mensagem = `Você encontrou uma espada melhor! Sua força aumentou em ${ganhoForca}.`;
                 opcao1 = "Pegar a espada";
-                opcao2 = "Deixar a espada";
-                break;
-
-            case 'perda_medicamentos':
-                const perdaMedicamentos = Math.floor(Math.random() * 8) + 2; 
-                this.medicamentos -= perdaMedicamentos;
-                mensagem = `Você foi roubado e perdeu ${perdaMedicamentos} medicamentos.`;
-                opcao1 = "Enfrentá-los";
-                opcao2 = "Fugir e perder ainda mais medicamentos";
-                break;
-
-            case 'sala_armadilha':
-                const danoArmadilha = Math.floor(Math.random() * 15) + 5;
-                this.vida -= danoArmadilha;
-                mensagem = `Você caiu em uma armadilha e perdeu ${danoArmadilha} de vida.`;
-                opcao1 = "Tentar desarmar a armadilha";
-                opcao2 = "Ignorar e continuar";
+                opcao2 = "Deixar a espada e avançar para o próximo andar";
                 break;
 
             case 'usar_medicamento':
-                const vidaRecuperada = Math.min(this.medicamentos * 5, 30); // Recupera até 30 de vida
+                const vidaRecuperada = Math.min(this.medicamentos * 5, 30);
                 this.vida += vidaRecuperada;
                 this.medicamentos -= Math.ceil(vidaRecuperada / 5);
                 mensagem = `Você usou medicamentos e recuperou ${vidaRecuperada} de vida.`;
-                opcao1 = "Continuar";
-                opcao2 = "Explorar mais";
+                opcao1 = "Explorar a sala";
+                opcao2 = "Avançar para o próximo andar";
                 break;
         }
 
@@ -99,6 +87,24 @@ class Personagem {
         botao1.innerText = opcao1;
         botao2.innerText = opcao2;
         modal.style.display = "flex";
+    }
+
+    realizarCombate() {
+        const chanceDeVitoria = Math.random() * 100;
+        const chanceDePerdaMedicamentos = Math.floor(Math.random() * 5) + 2;
+
+        if (chanceDeVitoria < this.forca) {
+            console.log("Você venceu o combate! Não perdeu nada.");
+        } else {
+            this.medicamentos -= chanceDePerdaMedicamentos;
+            console.log(`Você perdeu a batalha e perdeu ${chanceDePerdaMedicamentos} medicamentos.`);
+        }
+    }
+
+    fugir() {
+        const perdaMedicamentos = Math.floor(Math.random() * 3) + 1;
+        this.medicamentos -= perdaMedicamentos;
+        console.log(`Você fugiu e perdeu ${perdaMedicamentos} medicamentos.`);
     }
 
     statusAtual() {
@@ -140,8 +146,13 @@ function executarAcao(acao) {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
 
+    if (acao === "Atacá-los") {
+        personagem.realizarCombate();
+    } else if (acao === "Fugir") {
+        personagem.fugir();
+    }
+
     console.log(`Ação escolhida: ${acao}`);
 
-    // Após o jogador escolher uma ação, continua para o próximo evento
     personagem.mostrarProximoEvento();
 }
